d10287c8a72bcd44334d136c90c2b9724f54b5b3
Mike Turley <mike.turley@alum.cs.umass.edu>
2023-11-15 15:42:53 -0500

    Clean up type assertions
    Signed-off-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- packages/module/src/hooks/useClientTableBatteries.ts
+++ packages/module/src/hooks/useClientTableBatteries.ts
@@ -1,11 +1,5 @@
 import { useTablePropHelpers } from './useTablePropHelpers';
-import {
-  GetClientTableDerivedStateArgs,
-  TableBatteries,
-  TableState,
-  UseClientTableBatteriesArgs,
-  UseTablePropHelpersArgs
-} from '../types';
+import { TableBatteries, TableDerivedState, TableState, UseClientTableBatteriesArgs } from '../types';
 import { getClientTableDerivedState } from './getClientTableDerivedState';
 import { useTableState } from './useTableState';
 
@@ -26,27 +20,20 @@ export const useClientTableBatteries = <
 ): TableBatteries<TItem, TColumnKey, TSortableColumnKey, TFilterCategoryKey, TPersistenceKeyPrefix> => {
   const state = useTableState(args);
 
-  // TODO figure out why the `as` below is necessary, the `{ ...args, ...state }` should be enough for TS to infer the rest
-  // TODO do we have an actual issue with types here or is this a limitation of TS inference?
-  const derivedState = getClientTableDerivedState({ ...args, ...state } as TableState<
+  type Args = UseClientTableBatteriesArgs<
     TItem,
     TColumnKey,
     TSortableColumnKey,
     TFilterCategoryKey,
     TPersistenceKeyPrefix
-  > &
-    GetClientTableDerivedStateArgs<TItem, TColumnKey, TSortableColumnKey, TFilterCategoryKey>);
-
-  const argsForGetClientTableDerivedState = { ...args, ...state };
-  const argsForUseTablePropHelpers = { ...args, ...state, ...derivedState };
-  // eslint-disable-next-line no-console
-  console.log({ argsForGetClientTableDerivedState, argsForUseTablePropHelpers });
+  >;
+  type State = TableState<TItem, TColumnKey, TSortableColumnKey, TFilterCategoryKey, TPersistenceKeyPrefix>;
 
   // TODO figure out why the `as` below is necessary, the `{ ...args, ...state }` should be enough for TS to infer the rest
   // TODO do we have an actual issue with types here or is this a limitation of TS inference?
-  return useTablePropHelpers({
-    ...args,
-    ...state,
-    ...derivedState
-  } as UseTablePropHelpersArgs<TItem, TColumnKey, TSortableColumnKey, TFilterCategoryKey, TPersistenceKeyPrefix>);
+  const derivedState = getClientTableDerivedState({ ...args, ...state } as Args & State);
+
+  // TODO figure out why the `as` below is necessary, the `{ ...args, ...state }` should be enough for TS to infer the rest
+  // TODO do we have an actual issue with types here or is this a limitation of TS inference?
+  return useTablePropHelpers({ ...args, ...state, ...derivedState } as Args & State & TableDerivedState<TItem>);
 };
